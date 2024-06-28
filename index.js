3686c509a98d602a569bb07d485bef0e45b8aa2d
june jepkirui <jepkiruijune@gmail.com>
2024-06-28 16:37:38 +0300

    Done With Assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+	let message = [];
+	for (let i = 0; i < names.length; i++) {
+		message.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+	}
+	return message;
+}
+console.log(writeCards(names));
+
+
+function countDown(i) {
+  while (i >= 0) {
+      console.log(i);
+      i--;
+  }
+}
+countDown(10);
